ac5fa77051ea989eff0ef3c8671878c0c74c5107
Corrie Robbertse <corrie.robbertse@healthalliance.co.nz>
2023-08-08 13:15:51 +1200

    updated outputs

--- vnet/outputs.tf
+++ vnet/outputs.tf
@@ -1,7 +1,7 @@
 output "id" {
-    value = azurerm_virtual_network.this.id
+    value = azurerm_virtual_network.vnet.id
 }
 
 output "name" {
-    value = azurerm_virtual_network.this.name
+    value = azurerm_virtual_network.vnet.name
 }
--- vnet/peering.tf
+++ vnet/peering.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network_peering" "hub" {
   count = var.peer_type == "hub" ? 1 : 0  
   name                      = "hub-to-${var.remote_vnet_name}"
   resource_group_name       = var.resource_group_name
-  virtual_network_name      = azurerm_virtual_network.this.name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
   remote_virtual_network_id = var.remote_vnet_id
   allow_gateway_transit     = true
 }
@@ -11,7 +11,7 @@ resource "azurerm_virtual_network_peering" "peer" {
   count = var.peer_type == "peer" ? 1 : 0  
   name                      = "peer-to-${var.remote_vnet_name}"
   resource_group_name       = var.resource_group_name
-  virtual_network_name      = azurerm_virtual_network.this.name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
   remote_virtual_network_id = var.remote_vnet_id
   use_remote_gateways       = true
 }
